011fb35db993c48e883bdda9890c92f239ac1beb
Andrey Kochnov <andreykochnov@yandex.ru>
2020-10-04 00:19:59 +0300

    api

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_ADDRESS = 'http://predev.nl-mail.ru';
+export const BACKEND_ADDRESS = 'http://predev.nl-mail.ru/api';
 // export const BACKEND_ADDRESS = 'http://localhost:8099';
